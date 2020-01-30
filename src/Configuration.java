b319b57af6b800cd9cac710907c0036cbbf4edd9
TscheyDee <dietzel.jandavid@gmail.com>
2020-01-30 17:01:14 +0100

    Components included

---
+++ src/Configuration.java
@@ -0,0 +1,5 @@
+public class Configuration {
+
+    public final static SearchAlgorithm searchAlgorithm = SearchAlgorithm.NATIVE;
+
+}
--- src/IEmployeeManagement.java
+++ src/IEmployeeManagement.java
@@ -3,5 +3,4 @@ public interface IEmployeeManagement extends IROEmployeeManagement {
     public void createEmployee(String name, String type);
 
     public void assignIDCard(IDCardEmployee idCard, Employee employee);
-
 }
